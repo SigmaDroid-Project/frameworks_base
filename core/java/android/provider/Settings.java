f2498dc81ad7a85e942cf1b950418476803f5423
Shubham <skpawar1305@gmail.com>
2021-12-04 22:57:16 +0800

    base: Toggle vertical swipes on Ambient display [1/2]
    with single tap to ambient display on, many-a-times there will be accidental
    swipes on display after tap, opening keyguard, prevent this sequence like OxygenOS
    
    * keep it optional
    
    Change-Id: I9c0cf8b7434eca62e5c9e94c64496d6a2c5be5c0
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6769,6 +6769,12 @@ public final class Settings {
         public static final String FLASHLIGHT_ON_CALL_RATE = "flashlight_on_call_rate";
 
         /**
+         * Enable\Disable swipe up or down gesture on ambient display like OOS
+         * @hide
+         */
+        public static final String DOZE_AMBIENT_SWIPE = "doze_ambient_swipe";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
