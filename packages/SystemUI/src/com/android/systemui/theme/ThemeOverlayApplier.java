b9504874c9cb7982108aa47042a3b963dda6eae3
elpaablo <donopwhile1@gmail.com>
2023-06-06 18:03:01 +0100

    Revert "SystemUI: Add brightness slider styles [1/3]"
    This reverts commit fc2ba12a57abcac3db160f24df96f6faeeef6735.

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -140,11 +140,6 @@ public class ThemeOverlayApplier implements Dumpable {
             "android.theme.customization.qs_panel";
 
 
-    static final String OVERLAY_BRIGHTNESS_SLIDER_FILLED =
-            "com.android.systemui.brightness_slider.filled";
-    static final String OVERLAY_BRIGHTNESS_SLIDER_THIN =
-            "com.android.systemui.brightness_slider.thin";
-
     /*
      * All theme customization categories used by the system, in order that they should be applied,
      * starts with launcher and grouped by target package.
@@ -181,12 +176,6 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_NAVBAR,
             OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
             OVERLAY_CATEGORY_QS_PANEL);
-
-    /* Brightness slider overlays */
-    static final List<String> BRIGHTNESS_SLIDER_OVERLAYS = Lists.newArrayList(
-            "",
-            OVERLAY_BRIGHTNESS_SLIDER_FILLED,
-            OVERLAY_BRIGHTNESS_SLIDER_THIN);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -303,22 +292,6 @@ public class ThemeOverlayApplier implements Dumpable {
             }
         });
     }
-
-    /* Set brightness slider styles */
-    public void setBrightnessSliderStyle(int brightnessSliderStyle) {
-        mBgExecutor.execute(() -> {
-            try {
-                for (int i = 1; i < BRIGHTNESS_SLIDER_OVERLAYS.size(); i++) {
-                    String overlay = BRIGHTNESS_SLIDER_OVERLAYS.get(i);
-                    boolean enable = (i == brightnessSliderStyle);
-                    mOverlayManager.setEnabled(overlay, enable, UserHandle.SYSTEM);
-                }
-            } catch (SecurityException | IllegalStateException e) {
-                Log.e(TAG, "Failed to set brightness slider style", e);
-            }
-        });
-    }
-
 
     @VisibleForTesting
     protected OverlayManagerTransaction.Builder getTransactionBuilder() {
