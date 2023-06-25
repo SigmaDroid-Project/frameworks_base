32e568f2b3ce27bff012e1ae9d251bc98d690a94
Joey Huab <joey@evolution-x.org>
2023-06-25 20:22:12 +0900

    PixelPropsUtils: Refactor
    * Allows some devices to fully pass safetynet's strong attestation (maybe due to prebuilt vendor)
    * Do not spoof restore, pixelmigrate and setupwizard
    * Enable Photos spoof by default
    * Keep Photos happy and not count towards storage quota
    * Separate GamesProps into GamesPropsUtils
    * Switch from POCO F4 to POCO F5
    * Transfer GMS spoofing from AttestationHooks
    
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Gustavo Mendes <gusttavo.me@outlook.com>
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -275,7 +275,7 @@ import com.android.server.usage.AppStandbyInternal;
 
 import org.lineageos.internal.applications.LineageActivityManager;
 
-import com.android.internal.util.evolution.AttestationHooks;
+import com.android.internal.util.sigma.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1992,7 +1992,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("getFocusedRootTaskInfo()");
         }
         final long ident = Binder.clearCallingIdentity();
@@ -3024,7 +3024,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("registerTaskStackListener()");
         }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
