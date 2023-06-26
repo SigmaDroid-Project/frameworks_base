10d34a016b5edc34080b3c2024974c0c6523dd3d
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-06-26 19:37:17 +0700

    SystemUI:Fixup: Re-evaluate theme when changing VoLTE or VoWiFi Icons
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -620,6 +620,15 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                         @Override
                         public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
                                 int userId) {
+                            if (DEBUG) Log.d(TAG, "Overlay changed for user: " + userId);
+                            if (mUserTracker.getUserId() != userId) {
+                                return;
+                        }
+                            if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                                Log.i(TAG, "Theme application deferred when setting changed.");
+                                mDeferredThemeEvaluation = true;
+                                return;
+                            }
                             reevaluateSystemTheme(true /* forceReload */);
                         }
                     },
@@ -632,6 +641,15 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                         @Override
                         public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
                                 int userId) {
+                            if (DEBUG) Log.d(TAG, "Overlay changed for user: " + userId);
+                            if (mUserTracker.getUserId() != userId) {
+                                return;
+                        }
+                            if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                                Log.i(TAG, "Theme application deferred when setting changed.");
+                                mDeferredThemeEvaluation = true;
+                                return;
+                            }
                             reevaluateSystemTheme(true /* forceReload */);
                         }
                     },
