d89ee6a0856b062c69fa966606e4494e2f9e1f37
Matt Filetto <matt.filetto@gmail.com>
2023-07-27 19:02:42 -0700

    Revert "Introduce PixelPropsUtils for safety net spoof [SQUASHED]"
    This reverts commit 9b365fcf2dede92ff36184b36eccb9a8638d185e.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -65,6 +65,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.sigma.GamesPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1243,7 +1245,8 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PropImitationHooks.setProps(app);
+        String packageName = context.getPackageName();
+        GamesPropsUtils.setProps(context);
         return app;
     }
 
@@ -1261,7 +1264,8 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PropImitationHooks.setProps(app);
+        String packageName = context.getPackageName();
+        GamesPropsUtils.setProps(context);
         return app;
     }
 
