210375e72b7a5a22577cc174644abea218f2e10c
Joey Huab <joey@evolution-x.org>
2023-08-17 19:33:07 +0900

    PixelPropsUtils: Refactor again
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -274,8 +274,8 @@ import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.usage.AppStandbyInternal;
 
 import org.lineageos.internal.applications.LineageActivityManager;
-
 import com.android.internal.util.sigma.PixelPropsUtils;
+import com.android.internal.util.sigma.AttestationHooks;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1992,7 +1992,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("getFocusedRootTaskInfo()");
         }
         final long ident = Binder.clearCallingIdentity();
@@ -3024,14 +3024,14 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("registerTaskStackListener()");
         }
-        mTaskChangeNotificationController.registerTaskStackListener(listener);
+mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
     /** Unregister a task stack listener so that it stops receiving callbacks. */
-    @Override
+@Override
     public void unregisterTaskStackListener(ITaskStackListener listener) {
         enforceTaskPermission("unregisterTaskStackListener()");
         mTaskChangeNotificationController.unregisterTaskStackListener(listener);
