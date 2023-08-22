529b1a6a3b4da54c6a47d08fc5e5428201c60934
Matt Filetto <matt.filetto@gmail.com>
2023-08-21 23:20:24 -0700

    udfps:: only trigger onFingerDown with regular action down events under certain conditions.
    Change 1aa9c6e69de5156daa463dfe068444822155629e is not good for all devices, so use an overlay to control whether
    to trigger onFingerDown with regular action down events.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -617,12 +617,14 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                     // We need to persist its ID to track it during ACTION_MOVE that could include
                     // data for many other pointers because of multi-touch support.
                     mActivePointerId = event.getPointerId(0);
-                    final int idx = mActivePointerId == -1
-                            ? event.getPointerId(0)
-                            : event.findPointerIndex(mActivePointerId);
                     mVelocityTracker.addMovement(event);
-                    onFingerDown(requestId, (int) event.getRawX(), (int) event.getRawY(),
-                            (int) event.getTouchMinor(idx), (int) event.getTouchMajor(idx));
+                    if (shouldTriggerOnFingerDownWithActionDown()) {
+                        final int idx = mActivePointerId == -1
+                                ? event.getPointerId(0)
+                                : event.findPointerIndex(mActivePointerId);
+                        onFingerDown(requestId, (int) event.getRawX(), (int) event.getRawY(),
+                                (int) event.getTouchMinor(idx), (int) event.getTouchMajor(idx));
+                    }
                     handled = true;
                     mAcquiredReceived = false;
                 }
@@ -731,6 +733,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 // Do nothing.
         }
         return handled;
+    }
+
+    private boolean shouldTriggerOnFingerDownWithActionDown() {
+        return mContext.getResources().getBoolean(
+                R.bool.config_onFingerDownWithActionDown);
     }
 
     private boolean shouldTryToDismissKeyguard() {
