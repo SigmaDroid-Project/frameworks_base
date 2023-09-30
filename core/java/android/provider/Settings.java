8248c93bfb6c4be1b60781cd692c79b9ff80262e
Matt Filetto <matt.filetto@gmail.com>
2023-09-29 18:39:11 -0700

    Revert "SystemUI: Add Resizable QS header Space[1/2]"
    This reverts commit 8cc9810bffcd5f6f15f56982769e9a63efebf40c.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6511,12 +6511,6 @@ public final class Settings {
          */
         public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
 
-        /**
-          * QS header size
-          * @hide
-          */
-        public static final String QS_HEADER_SIZE = "qs_header_size";
-
        /**
          * These are all public system settings
          *
@@ -6675,7 +6669,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_PADDING_TOP);
             PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_URI);
             PRIVATE_SETTINGS.add(SHOW_BRIGHTNESS_BAR_PERCENTAGE);
-            PRIVATE_SETTINGS.add(QS_HEADER_SIZE);
         }
 
         /**
