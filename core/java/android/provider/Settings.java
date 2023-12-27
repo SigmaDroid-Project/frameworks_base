4c45364132fdcabdda9b043c6c2810e450d8fbd4
Matt Filetto <matt.filetto@gmail.com>
2023-12-27 05:22:22 -0800

    Revert "Add Alert Slider user interface [SQUASHED]"
    This reverts commit 32cdca15ec98277b0d0052218f112affcc55b4c0.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6316,12 +6316,6 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
-         * Whether to show or hide alert slider notifications on supported devices
-         * @hide
-         */
-        public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
-
-        /**
          * Whether to show heads up only for dialer and sms apps
          * @hide
          */
