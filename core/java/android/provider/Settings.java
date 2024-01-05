0bc7d5462de96764a40c2b01550710720564cf6b
LurdRaiden <everethduncan@gmail.com>
2024-01-05 13:49:10 -0500

    Introduce Dashboard Style
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6657,6 +6657,11 @@ public final class Settings {
          */
         public static final String SETTINGS_STYLE = "settings_style";
 
+         /**
+         * @hide
+         */
+         public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+
         /**
          * Which Vibration Pattern to use
          * 0: dzzz-dzzz
