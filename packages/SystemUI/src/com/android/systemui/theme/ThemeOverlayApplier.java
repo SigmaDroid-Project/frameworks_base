b860a19d21eb48a991ac6fcbd411f5a54aed7b3c
elpaablo <donopwhile1@gmail.com>
2024-03-10 09:48:36 +0000

    Drop UI style sysui
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -129,9 +129,6 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_UI_STYLE_SETTINGS =
             "android.theme.customization.style.settings";
-    @VisibleForTesting
-    static final String OVERLAY_CATEGORY_UI_STYLE_SYSUI =
-            "android.theme.customization.style.systemui";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -153,8 +150,7 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_NAVBAR,
             OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
             OVERLAY_CATEGORY_UI_STYLE_ANDROID,
-            OVERLAY_CATEGORY_UI_STYLE_SETTINGS,
-            OVERLAY_CATEGORY_UI_STYLE_SYSUI);
+            OVERLAY_CATEGORY_UI_STYLE_SETTINGS);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -165,7 +161,6 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_FONT,
             OVERLAY_CATEGORY_SHAPE,
             OVERLAY_CATEGORY_UI_STYLE_ANDROID,
-            OVERLAY_CATEGORY_UI_STYLE_SYSUI,
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_NAVBAR,
@@ -199,7 +194,7 @@ public class ThemeOverlayApplier implements Dumpable {
                 OVERLAY_CATEGORY_FONT, OVERLAY_CATEGORY_SHAPE,
                 OVERLAY_CATEGORY_ICON_ANDROID, OVERLAY_CATEGORY_UI_STYLE_ANDROID));
         mTargetPackageToCategories.put(SYSUI_PACKAGE,
-                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI, OVERLAY_CATEGORY_UI_STYLE_SYSUI ));
+                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI));
         mTargetPackageToCategories.put(SETTINGS_PACKAGE,
                 Sets.newHashSet(OVERLAY_CATEGORY_ICON_SETTINGS, OVERLAY_CATEGORY_UI_STYLE_SETTINGS));
         mTargetPackageToCategories.put(mLauncherPackage,
@@ -212,7 +207,6 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_SHAPE, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_UI_STYLE_ANDROID, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_UI_STYLE_SETTINGS, SETTINGS_PACKAGE);
-        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_UI_STYLE_SYSUI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_ANDROID, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SYSUI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SETTINGS, SETTINGS_PACKAGE);
