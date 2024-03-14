b2c4e13257b29fe1b25892af9d26461eb3ed6c26
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 18:48:07 +0800

    SystemUI: Implement intensity-based brightness slider haptics
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -64,6 +64,8 @@ import dagger.assisted.AssistedInject;
 
 import java.util.concurrent.Executor;
 
+import com.android.internal.util.android.VibrationUtils;
+
 public class BrightnessController implements ToggleSlider.Listener, MirroredBrightnessController,
         TunerService.Tunable {
 
@@ -116,7 +118,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private Vibrator mVibrator;
     private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
             VibrationEffect.get(VibrationEffect.EFFECT_TICK);
-    private boolean mBrightnessSliderHaptic;
+    private int mBrightnessSliderHaptic;
     private final TunerService mTunerService;
 
     @Override
@@ -135,7 +137,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                 break;
             case QS_BRIGHTNESS_SLIDER_HAPTIC:
                 mBrightnessSliderHaptic =
-                        TunerService.parseIntegerSwitch(newValue, false);
+                        TunerService.parseInteger(newValue, 0);
                 break;
             default:
                 break;
@@ -377,8 +379,8 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         setBrightness(valFloat);
 
         // Give haptic feedback only if brightness is changed manually
-        if (mBrightnessSliderHaptic && mVibrator != null && tracking)
-            mVibrator.vibrate(BRIGHTNESS_SLIDER_HAPTIC);
+        if (mBrightnessSliderHaptic > 0 && mVibrator != null && tracking)
+            VibrationUtils.triggerVibration(mContext, mBrightnessSliderHaptic);
 
         if (!tracking) {
             AsyncTask.execute(new Runnable() {
