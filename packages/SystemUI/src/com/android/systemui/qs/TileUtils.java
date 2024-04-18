fcec2efde789dfbd367c4ce9733be803ff609b1b
Matt Filetto <matt.filetto@gmail.com>
2024-04-18 01:02:57 -0700

    QSTile: fixup
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/TileUtils.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileUtils.java
@@ -80,7 +80,7 @@ public class TileUtils {
     }
 
     public static float getQSTileLabelSize(Context context) {
-        return (float) Settings.System.getIntForUser(context.getContentResolver(),
+        int labelSize = Settings.System.getIntForUser(context.getContentResolver(),
                 Settings.System.QS_TILE_LABEL_SIZE,
                 15, UserHandle.USER_CURRENT);
         if (getQsUiStyle(context) != 0) labelSize = labelSize - 2;
