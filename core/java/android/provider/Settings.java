ef3a0791e48b949ddc0295bf8360f47b1fb40d05
Matt Filetto <matt.filetto@gmail.com>
2024-04-25 16:13:17 -0700

    Revert "Battery charge warning [1/2]"
    This reverts commit 903e61ae315cda323028152eb10ea58e3e48dcb6.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14138,12 +14138,6 @@ public final class Settings {
         public static final String CHARGING_VIBRATION_ENABLED = "charging_vibration_enabled";
 
         /**
-         * Ringtone for battery charge level warning.
-         * @hide
-         */
-        public static final String BATTERY_LEVEL_CHARGE_SOUND_ALARM = "battery_level_charge_ringtone";
-
-        /**
          * Whether we keep the device on while the device is plugged in.
          * Supported values are:
          * <ul>
