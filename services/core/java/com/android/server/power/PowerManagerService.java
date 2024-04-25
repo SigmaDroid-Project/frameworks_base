47fb31086b9a01e05c4cd6e221b7bd8ed1e268e8
Matt Filetto <matt.filetto@gmail.com>
2024-04-25 16:10:49 -0700

    enable debug and always let suspend
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -169,7 +169,7 @@ public final class PowerManagerService extends SystemService
         implements Watchdog.Monitor {
     private static final String TAG = "PowerManagerService";
 
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
     private static final boolean DEBUG_SPEW = DEBUG && true;
 
     // Message: Sent when a user activity timeout occurs to update the power state.
@@ -4051,7 +4051,7 @@ public final class PowerManagerService extends SystemService
         // transition to DOZING isn't considered "changed" until the doze wake lock is
         // acquired.
         if (getGlobalWakefulnessLocked() == WAKEFULNESS_DOZING && mDozeStartInProgress) {
-            return true;
+            return false;
         }
 
         for (int idx = 0; idx < mPowerGroups.size(); idx++) {
