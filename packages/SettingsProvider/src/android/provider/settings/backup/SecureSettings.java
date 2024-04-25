96acccaadce097f31e24c840d34e67400e5d3348
Matt Filetto <matt.filetto@gmail.com>
2024-04-25 16:21:27 -0700

    Revert "base: Extend showing Ambient instead of fully waking to single tap gesture [1/2]"
    This reverts commit b92b6692caf3ecff3aeb44ce4858a4181863fa1d.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -262,7 +262,6 @@ public class SecureSettings {
         Settings.Secure.EXTRA_DIM_AUTO_TIME,
         Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
-        Settings.Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT,
         Settings.Secure.CLOCK_LS,
         Settings.Secure.MEDIA_CONTROLS_ALWAYS_SHOW_TIME
     };
