c70d950e20766958d03ed31d12cfe9fe9e0abe0c
elpaablo <donopwhile1@gmail.com>
2024-06-13 06:56:11 +0100

    QS panel styles cleanup

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -279,7 +279,6 @@ open class QSTileViewImpl @JvmOverloads constructor(
 
         background = createTileBackground()
         setColor(backgroundColor)
-        setOverlayColor(backgroundOverlayColor)
     }
 
     private fun createAndAddLabels() {
