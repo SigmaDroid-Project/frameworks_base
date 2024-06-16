2c799e3e1632cd33ee72fdc119a6a4df43b5801d
elpaablo <donopwhile1@gmail.com>
2024-06-16 16:02:15 +0100

    Volume panel styles framework support
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -131,6 +131,9 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_NF_STYLE =
             "android.theme.customization.nf_style";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_VOLUME_STYLE =
+            "android.theme.customization.volume_style";
 
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_UI_STYLE_ANDROID =
@@ -170,6 +173,7 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_QS_STYLE,
             OVERLAY_CATEGORY_BB_STYLE,
             OVERLAY_CATEGORY_NF_STYLE,
+            OVERLAY_CATEGORY_VOLUME_STYLE,
             OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT,
             OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT);
 
@@ -189,6 +193,7 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_QS_STYLE,
             OVERLAY_CATEGORY_BB_STYLE,
             OVERLAY_CATEGORY_NF_STYLE,
+            OVERLAY_CATEGORY_VOLUME_STYLE,
             OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT,
             OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT);
 
@@ -245,6 +250,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_QS_STYLE, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_BB_STYLE, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NF_STYLE, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_VOLUME_STYLE, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT, ANDROID_PACKAGE);
 
