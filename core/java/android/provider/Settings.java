c8df1740a911a742ac03f93278de8317d9741a07
Matt Filetto <matt.filetto@gmail.com>
2024-09-12 18:45:58 -0700

    Revert "Add Alert Slider user interface [SQUASHED]"
    This reverts commit c9ca833e58f2a71cd77b46a317b382bb3a11a2be.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6773,12 +6773,6 @@ public final class Settings {
         public static final String NAVBAR_STYLE = "navbar_style";
 
         /**
-         * Whether to show or hide alert slider notifications on supported devices
-         * @hide
-         */
-        public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
-
-        /**
          * Whether to show heads up only for dialer and sms apps
          * @hide
          */
